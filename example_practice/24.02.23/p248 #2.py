b3f6a92ab98724a888baaa823f83d7c4a82f5593
Seongmin Park <mm03170411@gmail.com>
2024-02-25 01:48:54 +0900

    example_0223

--- example_practice/24.02.23/p248 #2.py
+++ example_practice/24.02.23/p248 #2.py
@@ -1,10 +1,8 @@
 key_list = ["name", "hp", "mp", "level"]
 value_list = ["기사", 200, 30, 5]
-character = {
-    "name" : "기사",
-    "hp" : 200,
-    "mp" : 30,
-    "level" : 5
-    }
-# 내가 푼건 직접 입력한 것이고, for문을 사용해서 다시 해볼것 
+character = {}
+
+for i in range(0, len(key_list)):
+    character[key_list[i]] = value_list[i]
+    
 print(character)
