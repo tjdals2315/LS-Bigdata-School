952f3e3b7e03ad6131c2965a2540c159538238c9
Seongmin Park <mm03170411@gmail.com>
2024-02-25 14:52:42 +0900

    example_0223

--- example_practice/24.02.23/p266 #2.py
+++ example_practice/24.02.23/p266 #2.py
@@ -1,5 +1,9 @@
-# output = 1
+# 1-100사이 숫자 중 2진수로 변환했을 때 0이 하나만 포함된 숫자를 찾고, 그 숫자들의 합을 구하는 코드
+output = [i
+          for i in range(1, 101)
+          if "{:b}".format(i).count("0") == 1
+          ]
 
-# for i in :
-#     print("{} : {}".format(i, "{b}".format(i)))
-# print("합계:", sum(output))
+for i in output:
+    print("{} : {}".format(i, "{:b}".format(i)))
+print("합계", sum(output))
