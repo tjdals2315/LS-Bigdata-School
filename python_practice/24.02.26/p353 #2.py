806d94ee483e2a19b9d6be41cc68770475c7548a
Seongmin Park <144311844+tjdals2315@users.noreply.github.com>
2024-03-01 23:30:00 +0900

    practice_0226

--- python_practice/24.02.26/p353 #2.py
+++ python_practice/24.02.26/p353 #2.py
@@ -1,13 +1,13 @@
-# # lambda 사용
-# numbers = list(range(1, 10 + 1))
+# lambda 사용
+numbers = list(range(1, 10 + 1))
 
-# print("# 홀수만 추출하기")
-# print(list(filter(lambda x: x % 2 ,numbers)))
-# print()
+print("# 홀수만 추출하기")
+print(list(filter(lambda x: x % 2 == 1,numbers)))
+print()
 
-# print("#3 이상, 7 미만 추출하기")
-# print(list(filter(, numbers)))
-# print()
+print("#3 이상, 7 미만 추출하기")
+print(list(filter(lambda x: 3 <= x < 7, numbers)))
+print()
 
-# print("#제곱해서 50 미만 추출하기")
-# print(list(filter(, numbers)))
+print("#제곱해서 50 미만 추출하기")
+print(list(filter(lambda x: x**2 < 50, numbers)))
